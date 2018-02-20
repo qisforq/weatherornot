5766d3478329f743d155ee1526aeaca3d35fa5af
manosko <32654968+manosko@users.noreply.github.com>
2018-02-20 15:56:21 -0500

    a few lines

---
+++ client/src/components/Users.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'anon',
+      input: password
+    }
+  }
+
+  render () {
+    return (
+      <div>
+
+
+
+      </div>
+    )
+  }
+}
