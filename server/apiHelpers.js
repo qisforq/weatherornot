4afb252fbb7808b0bbd0fbd94cb812176f8913ac
Riley Alsman <rileyalsman@gmail.com>
2018-02-20 11:43:15 -0500

    Pull from origin/dev

--- server/apiHelpers.js
+++ server/apiHelpers.js
@@ -1,4 +1,5 @@
 module.exports = {
-	googleMapsAPI : 'AIzaSyCl-2Uv85jzATOsolRsSavBM7filgsFqkg',
-	darkSkyAPI : '1dc4337362add083bf9483261045a0c0'
-}
+  googleMapsAPI: 'AIzaSyCl-2Uv85jzATOsolRsSavBM7filgsFqkg',
+  darkSkyAPI: '1dc4337362add083bf9483261045a0c0',
+};
+
