475e71682fa2350a77cb760322534c6271d40f6c
Riley Alsman <rileyalsman@gmail.com>
2018-02-20 19:31:04 -0500

    Added routes file

--- server/index.js
+++ server/index.js
@@ -1,52 +1,51 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var axios = require('axios')
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 // var items = require('../database-mysql');
 
-var app = express();
-var apiHelpers = require('./apiHelpers.js')
-app.use(bodyParser.json())
-app.use(express.static(__dirname + '/../client/dist'));
+const app = express();
+const apiHelpers = require('./apiHelpers.js');
+
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../client/dist`));
 
 
+// GET REQUEST TO GOOGLEMAPS API FOR LAT/LONG/ ORIGIN/DESTINATION AND DISTANCE/TIME
+app.get('/search', (req, res) => {
+  const rootURL = 'https://maps.googleapis.com/maps/api/directions/json?origin=';
+  const origin = 'Hack Reactor NY';
+  const destination = 'Grand Central';
+  const URL = `${rootURL + origin}&destination=${destination}&key=${apiHelpers.googleMapsAPI}`;
 
-//GET REQUEST TO GOOGLEMAPS API FOR LAT/LONG/ ORIGIN/DESTINATION AND DISTANCE/TIME
-app.get('/search', function(req,res){
-	let rootURL = 'https://maps.googleapis.com/maps/api/directions/json?origin='
-	let origin = 'Hack Reactor NY'
-	let destination = 'Grand Central'
-	let URL = rootURL + origin + '&destination=' + destination + '&key=' + apiHelpers.googleMapsAPI
+  axios.get(URL).then((data) => {
+    console.log(data.data);
+    console.log('MapsAPI', data.data);
+    // res.send(data.data)
+    res.status(200).json(data.data);
+  });
+});
 
-	axios.get(URL).then((data) => {
-		console.log(data.data)
-      console.log('MapsAPI',data.data)
-      // res.send(data.data)
+
+// GET REQUEST TO DARK SKY API, IF NO LATITUDE OR LONGITUDE, RETURN PRESET
+app.get('/weather', (req, res) => {
+  const rootUrl = 'https://api.darksky.net/forecast';
+  const APIKey = apiHelpers.darkSkyAPI;
+  const lat = req.lat || '40.750487';
+  const long = req.lng || '-73.976401';
+  const requestUrl = `${rootUrl}/${APIKey}/${lat},${long}`;
+
+  axios.get(requestUrl)
+    .then((data) => {
       res.status(200).json(data.data);
-
-     })
-})
-
-
-//GET REQUEST TO DARK SKY API, IF NO LATITUDE OR LONGITUDE, RETURN PRESET
-app.get('/weather', function(req,res){
-	let rootUrl = 'https://api.darksky.net/forecast'
-	let APIKey = apiHelpers.darkSkyAPI
-	let lat = req.lat || '40.750487' 
- 	let long =  req.lng  || '-73.976401'
- 	let requestUrl = rootUrl + '/' + APIKey + '/' + lat + ',' + long;
-
-  	axios.get(requestUrl)
-	   .then(function(data) {
-	     res.status(200).json(data.data);
-	   })
-	   .catch(function(error) {
-	     console.log(error);
     })
-})
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
+app.get('/items', (req, res) => {
+  items.selectAll((err, data) => {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -54,7 +53,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(8080, function() {
+app.listen(8080, () => {
   console.log('listening on port 8080!');
 });
 
