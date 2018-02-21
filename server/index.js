6e916f59549d311261ce2b35baf02be5896dece2
Riley Alsman <rileyalsman@gmail.com>
2018-02-21 17:21:05 -0500

    Refactoring places post

--- server/index.js
+++ server/index.js
@@ -1,83 +1,92 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let api = require('./apiHelpers.js');
-let path = require('path')
-let db = require('../db/mysql.js')
-const config = require('./config.js')
+const express = require('express');
+const bodyParser = require('body-parser');
+const api = require('./apiHelpers.js');
+const path = require('path');
+const db = require('../db/mysql.js');
+const config = require('./config.js');
 const geocoder = require('google-geocoder')({
-  key: config.geocodeAPI
+  key: config.geocodeAPI,
 });
 
 
 // var items = require('../database-mysql');
-let app = express();
+const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/../client/dist')))
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-//Users
+// Users
 app.post('/users', (req, res) => {
 
-})
+});
 
-app.get('/users', (req,res) => {
+app.get('/users', (req, res) => {
 
-})
+});
 
 
-//Commutes
+// Commutes
 app.post('/commutes', (req, res) => {
 
-})
+});
 
 app.get('/commutes', (req, res) => {
 
-})
+});
 
 app.delete('/commutes', (req, res) => {
 
-})
+});
 
 
-//Places
+// Places
 app.post('/places', (req, res) => {
-  console.log("req.body", req.body);
-  if (req.body.lat && req.body.lng) {// If autocomplete has the lat and long coordinates, no need to make an api call
-    console.log("lat and long!", req.body.lat, req.body.lng);
-  } else if (req.body.address) {
-    geocoder.find(req.body.address, function(err, geoData){
+  // req.body = {address, placeType, lat, lng}
+
+  // if name of place exists, send back error
+
+  const {address, placeType, lat, lng} = req.body
+
+  
+
+  console.log('req.body', req.body);
+  if (lat && lng) {
+    const query = `INSERT INTO places (name, latitude, longitude, username) VALUES ("${placeType}", "${lat}", "${lng}", "${unQuery}")`
+    db.query(query, () => {
+
+    })
+  } else if (address) {
+    geocoder.find(address, (err, geoData) => {
       console.log('geocoder works');
-      let lat = 0;
-      let long = 0;
       if (geoData !== undefined) {
         lat = geoData[0].location.lat;
-        long = geoData[0].location.lng;
+        lng = geoData[0].location.lng;
       }
     });
   } else {
-    res.status(400).send(`Sorry, the address you submitted is not valid`);
+    res.status(400).send('Sorry, the address you submitted is not valid');
   }
-})
+});
 
 app.get('/places', (req, res) => {
 
-})
+});
 
 app.delete('/places', (req, res) => {
 
-})
+});
 
 
-//Timeline
+// Timeline
 app.get('/timeline', (req, res) => {
 
-})
+});
 
 
-//Status
+// Status
 app.get('/status', (req, res) => {
 
-})
+});
 
 app.listen(8080, () => {
   console.log('listening on port 8080!');
