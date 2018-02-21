bf51d8f078ba8d32317b41f7660a531e2e18548d
Riley Alsman <rileyalsman@gmail.com>
2018-02-20 20:56:10 -0500

    Reorganized server files, set up route scafolding, refined apihelpers

--- server/index.js
+++ server/index.js
@@ -1,73 +1,64 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var axios = require('axios')
-var geocoder = require('google-geocoder');
+let express = require('express');
+let bodyParser = require('body-parser');
+let api = require('./apiHelpers');
+let path = require('path')
+let db = require('../db/dbSchema')
+
 // var items = require('../database-mysql');
-var app = express();
-var apiHelpers = require('./apiHelpers.js')
-app.use(bodyParser.json())
-app.use(express.static(__dirname + '/../client/dist'));
+let app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(`${__dirname}/../client/dist`));
+app.use(express.static(path.join(__dirname, '/../client/dist')))
+
+//Users
+app.post('/users', (req, res) => {
+
+})
+
+app.get('/users', (req,res) => {
+  
+})
 
 
-// GET REQUEST TO GOOGLEMAPS API FOR LAT/LONG/ ORIGIN/DESTINATION AND DISTANCE/TIME
-app.get('/search', (req, res) => {
-  const rootURL = 'https://maps.googleapis.com/maps/api/directions/json?origin=';
-  const origin = 'Hack Reactor NY';
-  const destination = 'Grand Central';
-  const URL = `${rootURL + origin}&destination=${destination}&key=${apiHelpers.googleMapsAPI}`;
+//Commutes
+app.post('/commutes', (req, res) => {
 
-  axios.get(URL).then((data) => {
-    console.log(data.data);
-    console.log('MapsAPI', data.data);
-    // res.send(data.data)
-    res.status(200).json(data.data);
-  });
-});
+})
+
+app.get('/commutes', (req,res) => {
+
+})
+
+app.delete('/commutes', (req,res) => {
+
+})
 
 
-// GET REQUEST TO DARK SKY API, IF NO LATITUDE OR LONGITUDE, RETURN PRESET
-app.get('/weather', (req, res) => {
-  const rootUrl = 'https://api.darksky.net/forecast';
-  const APIKey = apiHelpers.darkSkyAPI;
-  const lat = req.lat || '40.750487';
-  const long = req.lng || '-73.976401';
-  const requestUrl = `${rootUrl}/${APIKey}/${lat},${long}`;
+//Places
+app.post('/places', (req, res) => {
 
-  axios.get(requestUrl)
-    .then((data) => {
-      res.status(200).json(data.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+})
 
- 
-var geo = geocoder({
-  key: apiHelpers.geocodeAPI
-});
- 
-geo.find('223 Edenbridge Dr, Toronto', function(err, res){
-	console.log(res)
-	console.log(err)
-  // process response object 
-});
+app.get('/places', (req,res) => {
+
+})
+
+app.delete('/places', (req,res) => {
+
+})
 
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+//Timeline
+app.get('/timeline', (req,res) => {
+
+})
+
+
+//Status
+app.get('/status', (req,res) => {
+
+})
 
 app.listen(8080, () => {
   console.log('listening on port 8080!');
 });
-
