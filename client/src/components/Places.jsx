f7e3454ebffa07f22e754f9f899e67e0e4895dc4
Quentin Vidal <qisforq@gmail.com>
2018-02-21 16:57:24 -0500

    /places post request near complete. Database access needed

--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PlaceItem from './PlaceItem.jsx';
 import config from '../../../server/config.js'
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+// import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import LocationAutocomplete from 'location-autocomplete';
 
 class Places extends React.Component {
@@ -11,23 +11,27 @@ class Places extends React.Component {
       address: '',
       placeType: 'home',
       showAddPlace: false,
+      lat: '',
+      lng: ''
     };
     this.onChange = this.onChange.bind(this);
     this.search = this.search.bind(this);
     this.toggleAddPlace = this.toggleAddPlace.bind(this);
-    this.onAutocompleteChange = this.onAutocompleteChange.bind(this)
+    // this.onAutocompleteChange = this.onAutocompleteChange.bind(this)
+    this.onDropdownSelect = this.onDropdownSelect.bind(this)
   }
 
   onChange(e) {
     // const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    // console.log("the name", e.target.name);
+    // console.log("the value", e.target.value);
     this.setState({ [e.target.name]: e.target.value });
   }
 
   search(e) {
     e.preventDefault();
-    let { address, placeType } = this.state;
-    this.props.sendAddress(address, placeType);
-    console.log('SEARCHED! Address:', address, 'placeType:', placeType);
+    let { address, placeType, lat, lng } = this.state;
+    this.props.sendAddress(address, placeType, lat, lng);
   }
 
   toggleAddPlace() {
@@ -37,8 +41,17 @@ class Places extends React.Component {
     });
   }
 
-  onAutocompleteChange(address) {
-    this.setState({address})
+  // onAutocompleteChange(address) {
+  //   this.setState({address})
+  // }
+
+  onDropdownSelect(e) {
+    let lat = e.autocomplete.getPlace().geometry.location.lat();
+    let lng = e.autocomplete.getPlace().geometry.location.lng();
+    this.setState({
+      lat: lat,
+      lng: lng
+    });
   }
 
   render() {
@@ -60,7 +73,7 @@ class Places extends React.Component {
                 <option value="home">Home</option>
                 <option value="work">Work</option>
               </select>
-              <PlacesAutocomplete inputProps={inputProps} />
+              {/* <PlacesAutocomplete inputProps={inputProps} /> */}
               {/* <input
                 name="address"
                 type="text"
@@ -71,11 +84,10 @@ class Places extends React.Component {
               <LocationAutocomplete
                 name="address"
                 placeholder="Enter Address"
-                targetArea="New York, NY"
-                locationType="(regions)"
-                googleAPIKey= config.geocodeAPI
-                onChange={() => {}}
-                onDropdownSelect={() => {}}
+                locationType="geocode"
+                googleAPIKey= "AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g"
+                onChange={(e) => {this.onChange(e)}}
+                onDropdownSelect={(e) => {this.onDropdownSelect(e)}}
               />
               <button type="submit">Submit</button>
             </form>
