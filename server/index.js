4ae6eb861f8e48a5742b846bccd979fd488fe764
Riley Alsman <rileyalsman@gmail.com>
2018-02-21 10:45:15 -0500

    Merge

--- server/index.js
+++ server/index.js
@@ -8,41 +8,42 @@ var apiHelpers = require('./apiHelpers.js')
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../client/dist`));
 
 
-//GET REQUEST TO GOOGLEMAPS API FOR LAT/LONG/ ORIGIN/DESTINATION AND DISTANCE/TIME
-app.get('/search', function(req,res){
-	let rootURL = 'https://maps.googleapis.com/maps/api/directions/json?origin='
-	let origin = 'Hack Reactor NY'
-	let destination = 'Grand Central'
-	let URL = rootURL + origin + '&destination=' + destination + '&key=' + apiHelpers.googleMapsAPI
+// GET REQUEST TO GOOGLEMAPS API FOR LAT/LONG/ ORIGIN/DESTINATION AND DISTANCE/TIME
+app.get('/search', (req, res) => {
+  const rootURL = 'https://maps.googleapis.com/maps/api/directions/json?origin=';
+  const origin = 'Hack Reactor NY';
+  const destination = 'Grand Central';
+  const URL = `${rootURL + origin}&destination=${destination}&key=${apiHelpers.googleMapsAPI}`;
 
-	axios.get(URL).then((data) => {
-		console.log(data.data)
-      console.log('MapsAPI',data.data)
-      // res.send(data.data)
+  axios.get(URL).then((data) => {
+    console.log(data.data);
+    console.log('MapsAPI', data.data);
+    // res.send(data.data)
+    res.status(200).json(data.data);
+  });
+});
+
+
+// GET REQUEST TO DARK SKY API, IF NO LATITUDE OR LONGITUDE, RETURN PRESET
+app.get('/weather', (req, res) => {
+  const rootUrl = 'https://api.darksky.net/forecast';
+  const APIKey = apiHelpers.darkSkyAPI;
+  const lat = req.lat || '40.750487';
+  const long = req.lng || '-73.976401';
+  const requestUrl = `${rootUrl}/${APIKey}/${lat},${long}`;
+
+  axios.get(requestUrl)
+    .then((data) => {
       res.status(200).json(data.data);
-
-     })
-})
-
-
-//GET REQUEST TO DARK SKY API, IF NO LATITUDE OR LONGITUDE, RETURN PRESET
-app.get('/weather', function(req,res){
-	let rootUrl = 'https://api.darksky.net/forecast'
-	let APIKey = apiHelpers.darkSkyAPI
-	let lat = req.lat || '40.750487' 
- 	let long =  req.lng  || '-73.976401'
- 	let requestUrl = rootUrl + '/' + APIKey + '/' + lat + ',' + long;
-
-  	axios.get(requestUrl)
-	   .then(function(data) {
-	     res.status(200).json(data.data);
-	   })
-	   .catch(function(error) {
-	     console.log(error);
     })
-})
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
  
 var geo = geocoder({
@@ -66,7 +67,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(8080, function() {
+app.listen(8080, () => {
   console.log('listening on port 8080!');
 });
 
