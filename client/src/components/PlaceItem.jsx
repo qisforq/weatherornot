a7097ea8dd9ee987b71bd3bd0ab3fe856dda49a3
Quentin Vidal <qisforq@gmail.com>
2018-02-22 19:29:53 -0500

    nothing important

--- client/src/components/PlaceItem.jsx
+++ client/src/components/PlaceItem.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 
 
 const PlaceItem = ({place, deletePlace}) => {
-  if (place) {
+
     return (
       <div
         className="place"
-        // onClick={(e) => {deletePlace(placeName)}}
         onClick={() => {
-          console.log('this is deleting ', place.name);
-          deletePlace(place)
+          console.log("PLACE:",place.weather);
+          // deletePlace(place)
         }}
         >
-        <label>{place.name}</label>
-        <div>~~pretendzies this is the "{place.name}" icon~~</div>
-      </div>
-    )
-  } else {
-    return <div></div>
-  }
+
+          <label>{place.name}</label>
+          <div>~~pretendzies this is the "{place.name}" icon~~</div>
+        </div>
+      )
+
 };
 
 export default PlaceItem;
