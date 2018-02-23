df1c831f4197d95abe94344b98d20fe7eea93e72
Quentin Vidal <qisforq@gmail.com>
2018-02-22 20:22:11 -0500

    Added weather icons

--- client/src/components/PlaceItem.jsx
+++ client/src/components/PlaceItem.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
-
+import Skycons from 'skycons'
+import ReactAnimatedWeather from 'react-animated-weather';
 
 const PlaceItem = ({place, deletePlace}) => {
+  // console.log("SKYCONS:", Skycons);
+  // console.log("sky:", sky.prototype);
+  // sky.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
+  const defaults = {
+    'clear-day': 'CLEAR_DAY',
+    'clear-night': 'CLEAR_NIGHT',
+    'partly-cloudy-day': 'PARTLY_CLOUDY_DAY',
+    'partly-cloudy-night': 'PARTLY_CLOUDY_NIGHT',
+    CLOUDY: 'cloudy',
+    'RAIN': 'rain',
+    'SLEET': 'sleet',
+    'SNOW': 'snow',
+    'WIND': 'wind',
+    'FOG': 'fog'
+  }
 
     return (
       <div
         className="place"
         onClick={() => {
-          console.log("PLACE:",place.weather);
-          // deletePlace(place)
+          console.log("PLACE:",);
         }}
         >
-
+          <ReactAnimatedWeather
+            icon={defaults[place.weather.current.icon]}
+            color={'black'}
+            size={64}
+            animate={true}
+          />
           <label>{place.name}</label>
-          <div>~~pretendzies this is the "{place.name}" icon~~</div>
         </div>
       )
 
