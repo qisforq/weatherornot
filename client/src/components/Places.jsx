d1d223821de48d306b11f8dbd39b592ca80acb50
Riley Alsman <rileyalsman@gmail.com>
2018-02-22 21:16:55 -0500

    Refactored Places

--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PlaceItem from './PlaceItem.jsx';
-import config from '../../../server/config.js'
+import config from '../../../server/config.js';
 // import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import LocationAutocomplete from 'location-autocomplete';
 
@@ -9,40 +9,47 @@ class Places extends React.Component {
     super(props);
     this.state = {
       address: '',
-      placeType: 'home',
-      showAddPlace: false,
+      showAddHome: false,
+      showAddWork: false,
       lat: '',
-      lng: ''
+      lng: '',
     };
     this.onChange = this.onChange.bind(this);
     this.search = this.search.bind(this);
     this.toggleAddPlace = this.toggleAddPlace.bind(this);
     // this.onAutocompleteChange = this.onAutocompleteChange.bind(this)
-    this.onDropdownSelect = this.onDropdownSelect.bind(this)
+    this.onDropdownSelect = this.onDropdownSelect.bind(this);
   }
 
   onChange(e) {
-    // const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-    // console.log("the name", e.target.name);
-    // console.log("the value", e.target.value);
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  search(e) {
+  search(e, placeType) {
     e.preventDefault();
-    let { address, placeType, lat, lng } = this.state;
+    const {
+      address, lat, lng,
+    } = this.state;
     this.props.sendAddress(address, placeType, lat, lng);
-    this.toggleAddPlace();
+    if (placeType === 'home') {
+      this.toggleAddPlace('Home');
+    } else {
+      this.toggleAddPlace('Work');
+    }
   }
 
-  toggleAddPlace() {
+  toggleAddPlace(locationType) {
+    console.log(locationType)
     // This function triggers the "Add a place" button to show the "enter address form"
-    console.log(this.props.places, "is this.props.places over a length of 2?");
+    if (locationType === 'Home') {
       this.setState({
-        showAddPlace: !this.state.showAddPlace,
+        showAddHome: !this.state.showAddHome,
       });
-    // {this.state.showButton ? `Add a place!` : `Sorry, currently we only allow two saved places`}
-
+    } else {
+      this.setState({
+        showAddWork: !this.state.showAddWork,
+      });
+    }
   }
 
   // onAutocompleteChange(address) {
@@ -50,80 +57,97 @@ class Places extends React.Component {
   // }
 
   onDropdownSelect(e) {
-    let lat = e.autocomplete.getPlace().geometry.location.lat();
-    let lng = e.autocomplete.getPlace().geometry.location.lng();
-    let adr = e.autocomplete.getPlace().formatted_address;
+    const lat = e.autocomplete.getPlace().geometry.location.lat();
+    const lng = e.autocomplete.getPlace().geometry.location.lng();
+    const adr = e.autocomplete.getPlace().formatted_address;
 
     this.setState({
-      lat: lat,
-      lng: lng,
-      address: adr
+      lat,
+      lng,
+      address: adr,
     });
   }
 
   render() {
-    if (this.props.places.length < 2) {
+    const home = this.props.places.find(place => place.name === 'home');
+    const work = this.props.places.find(place => place.name === 'work');
+
+    if (home && work) {
       return (
         <div>
-          <div className="placesBlock">
-            {this.props.places.map(place => <PlaceItem key={place.id} place={place} deletePlace={this.props.deletePlace}/>)}
-          </div>
-          <div className="addPlace">
-            {this.state.showAddPlace ? (
-              // if showAddPlace is true, then show the "enter address" form
-              <form onSubmit={this.search}>
-              {
-                this.props.places.length === 0 ?
-                <select name="placeType" value={this.state.placeType} onChange={(e) => { this.onChange(e); }}>
-                      <option value="home">Home</option>
-                      <option value="work">Work</option>
-                </select>
-                : this.props.places.placeType === 'home' ?
-                <select name="placeType" value={this.state.placeType} onChange={(e) => { this.onChange(e); }}>
-                      <option value="work">Work</option>
-                </select>
-                :
-                <select name="placeType" value={this.state.placeType} onChange={(e) => { this.onChange(e); }}>
-                      <option value="home">Home</option>
-                </select>
-
-              }
-                <LocationAutocomplete
-                  name="address"
-                  placeholder="Enter Address"
-                  locationType="geocode"
-                  googleAPIKey= "AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g"
-                  onChange={(e) => {this.onChange(e)}}
-                  onDropdownSelect={(e) => {this.onDropdownSelect(e)}}
-                />
-                <button type="submit">Submit</button>
-              </form>
-            ) : (
-              <button onClick={this.toggleAddPlace}>Add a place!</button>
-            )}
-          </div>
+          <PlaceItem key={home.id} place={home} deletePlace={this.props.deletePlace} />
+          <PlaceItem key={work.id} place={work} deletePlace={this.props.deletePlace} />
         </div>
       );
-    } else {
+    } else if (home && !work) {
       return (
         <div>
-          <div className="placesBlock">
-            {this.props.places.map((place, i) =>
-              <PlaceItem
-                key={place.id}
-                place={place}
-                deletePlace={this.props.deletePlace}
-                // onClick={(e) => {
-                //   console.log('this is onclick');
-                //   this.props.deletePlace(place)
-                // }}
+          <PlaceItem key={home.id} place={home} deletePlace={this.props.deletePlace} />
+          {this.state.showAddWork ?
+            <form onSubmit={e => this.search(e, 'work')}>
+              <LocationAutocomplete
+                name="address"
+                placeholder="Enter Address"
+                locationType="geocode"
+                googleAPIKey="AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g"
+                onChange={(e) => { this.onChange(e); }}
+                onDropdownSelect={(e) => { this.onDropdownSelect(e); }}
               />
-            )}
-          </div>
+              <button type="submit">Submit</button>
+            </form>
+            : <button onClick={(e)=>this.toggleAddPlace(e.target.innerText)}>Work</button>}
+        </div>
+      );
+    } else if (!home && work) {
+      return (
+        <div>
+          {this.state.showAddHome ?
+            <form onSubmit={e => this.search(e, 'home')}>
+              <LocationAutocomplete
+                name="address"
+                placeholder="Enter Address"
+                locationType="geocode"
+                googleAPIKey="AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g"
+                onChange={(e) => { this.onChange(e); }}
+                onDropdownSelect={(e) => { this.onDropdownSelect(e); }}
+              />
+              <button type="submit">Submit</button>
+            </form>
+            : <button onClick={(e)=>this.toggleAddPlace(e.target.innerText)}>Home</button>}
+          <PlaceItem key={work.id} place={work} deletePlace={this.props.deletePlace} />
         </div>
       );
     }
-
+    return (
+    <div>
+      {this.state.showAddHome ?
+        <form onSubmit={e => this.search(e, 'home')}>
+          <LocationAutocomplete
+            name="address"
+            placeholder="Enter Address"
+            locationType="geocode"
+            googleAPIKey="AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g"
+            onChange={(e) => { this.onChange(e); }}
+            onDropdownSelect={(e) => { this.onDropdownSelect(e); }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        : <button onClick={(e)=>this.toggleAddPlace(e.target.innerText)}>Home</button>}
+      {this.state.showAddWork ?
+        <form onSubmit={e => this.search(e, 'work')}>
+          <LocationAutocomplete
+            name="address"
+            placeholder="Enter Address"
+            locationType="geocode"
+            googleAPIKey="AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g"
+            onChange={(e) => { this.onChange(e); }}
+            onDropdownSelect={(e) => { this.onDropdownSelect(e); }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        : <button onClick={(e)=>this.toggleAddPlace(e.target.innerText)}>Work</button>}
+    </div>
+    );
   }
 }
 
--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -5,8 +5,8 @@ class Users extends React.Component {
     super(props);
     this.state = {
       toggleInput: true,
-      errors: []
-    }
+      errors: [],
+    };
   }
 
   usernameCheck(name) {
@@ -17,7 +17,7 @@ class Users extends React.Component {
       newErrors.push('Please use more than 3 characters')
     }
     if (name.match(/\W/)) {
-      newErrors\.push('Please only use numbers, letters and underscores')
+      newErrors.push('Please only use numbers, letters and underscores')
     }
     if (!name[0].match(/[a-zA-Z]/)) {
       newErrors.push('First character must be a letter')
