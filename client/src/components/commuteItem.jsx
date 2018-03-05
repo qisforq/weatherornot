a9ff75a1b149ecdaf156dae67f1e7aab2b69cf3a
Quentin Vidal <qisforq@gmail.com>
2018-03-05 15:13:42 -0500

    WORKS AND STYLED

--- client/src/components/commuteItem.jsx
+++ client/src/components/commuteItem.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const CommuteItem = ({ commute, deleteCommutes }) => {
-  console.log('haha',commute)
   if (commute.arriveordepart === 'A') {
     return (
-      <div>
+      <div className= 'col-sm-6' style={{textAlign: 'center'}}>
         <div></div>
         <div>{`Arriving at: ${commute.time}`}</div>
         <div>{`From ${commute.origin.name}`}</div>
@@ -22,11 +21,11 @@ const CommuteItem = ({ commute, deleteCommutes }) => {
     );
   } else if (commute.arriveordepart === 'D') {
     return (
-      <div>
-        <div>{`Departing at: ${commute.time}`}</div>
-        <div>{`From ${commute.origin.name}`}</div>
+      <div style={{textAlign: 'center', marginTop: '10px'}}>
+        <div>{`Departing at: ${commute.time}   From ${commute.origin.name}    To ${commute.destination.name}    Duration ${commute.travelTime.text}`}</div>
+        {/* <div>{`From ${commute.origin.name}`}</div>
         <div>{`To ${commute.destination.name}`}</div>
-        <div>{`Duration ${commute.travelTime.text}`}</div>
+        <div>{`Duration ${commute.travelTime.text}`}</div> */}
         <div>
           <button
             onClick={() => { deleteCommutes(commute); }}
--- client/src/components/commutes.jsx
+++ client/src/components/commutes.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import CommuteItem from './CommuteItem.jsx';
-import {TimePicker, RadioButtonGroup, RadioButton, DropDownMenu, MenuItem} from 'material-ui'
-import mUI from 'material-ui'
 
 class Commutes extends React.Component {
   constructor(props) {
@@ -22,38 +20,23 @@ class Commutes extends React.Component {
   }
 
   render() {
-    console.log('ahhhhhh', this.props.commutes)
     return (
       <div>
-        <div>
-
-        </div>
-        <TimePicker name="timePicker" placeholder="Time" />
-        <RadioButtonGroup name="AorD">
-          <RadioButton value="A" label="Arrive" />
-          <RadioButton value="D" label="Depart" />
-        </RadioButtonGroup>
-        <DropDownMenu name="dropDown">
-          <MenuItem value="A" name="Arrive" />
-          <MenuItem value="D" name="Depart" />
-        </DropDownMenu>
-        <div>
+        <br></br><br></br>
+        <div className = "row" style={{marginLeft: '12%'}}>
+        {this.state.aOrD === 'D' ? <button className = "col-sm-2" style={{backgroundColor:'rgb(5, 231, 255)', fontWeight: 500, borderRadius: '7px'}} onClick={()=> this.setState({aOrD: 'A'})}>Depart</button>
+        : <button  style={{backgroundColor:'rgb(5, 231, 255)', fontWeight: 500, borderRadius: '7px', marginLeft: '1%'}}  onClick={()=> this.setState({aOrD: 'D'})}>Arrive</button>}
           <input
-            name="deptOrArival"
-            type="checkbox"
-            value={this.state.deptOrArival}
-            deleteCommute={this.props.deleteCommute}
-            onChange={(e) => { this.onChange(e); }}
-          />
-        {this.state.aOrD === 'D' ? <button onClick={()=> this.setState({aOrD: 'A'})}>Depart</button>
-        : <button onClick={()=> this.setState({aOrD: 'D'})}>Arrive</button>}
-          <input
+            className = "col-sm-2"
+            style={{textAlign: 'center', fontWeight: 500, borderRadius: '7px', marginLeft: '1%'}}
             name="time"
             type="time"
             value={this.state.time}
             onChange={(e) => { this.onChange(e); }}
           />
           <input
+            className = "col-sm-2"
+            style={{textAlign: 'center', fontWeight: 500, borderRadius: '7px', marginLeft: '1%', marginBottom: '15px'}}
             name="org"
             type="select"
             placeholder={this.state.org || "origin"}
@@ -61,13 +44,15 @@ class Commutes extends React.Component {
             onChange={(e) => { this.onChange(e); }}
           />
           <input
+            className = "col-sm-2"
+            style={{textAlign: 'center', fontWeight: 500, borderRadius: '7px', marginLeft: '1%'}}
             name="dest"
             type="select"
             placeholder={this.state.destination || "destination"}
             value={this.state.dest}
             onChange={(e) => { this.onChange(e); }}
           />
-          <button onClick={()=> this.props.addCommuteHandler(this.state) } >Get All Commute Data</button>
+          <button className = "col-sm-2" onClick={()=> this.props.addCommuteHandler(this.state) } style ={{fontWeight: 500, backgroundColor:'rgb(5, 231, 255)', borderRadius: '7px', marginLeft: '1%'}}>Get Data</button>
         </div>
          <div>
           {this.props.commutes.map(commute =>
